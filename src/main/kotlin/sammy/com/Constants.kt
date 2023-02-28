e1f3e922038f666e739888cafda46a87271299f6
sammy-mutahi <sammymutahi21@gmail.com>
2023-02-28 18:54:56 +0300

    broadcast draw data

--- src/main/kotlin/sammy/com/Constants.kt
+++ src/main/kotlin/sammy/com/Constants.kt
@@ -2,5 +2,6 @@ package sammy.com
 
 object Constants {
     const val MAX_ROOM_SIZE = 8
-    const val TYPE_CHAT_MESSAGE = "chat message"
+    const val TYPE_CHAT_MESSAGE = "TYPE_CHAT_MESSAGE"
+    const val TYPE_DRAW_DATA = "DRAW_DATA"
 }
--- src/main/kotlin/sammy/com/routes/WebSocketRoutes.kt
+++ src/main/kotlin/sammy/com/routes/WebSocketRoutes.kt
@@ -7,11 +7,37 @@ import io.ktor.sessions.*
 import io.ktor.websocket.*
 import kotlinx.coroutines.channels.consumeEach
 import sammy.com.Constants
+import sammy.com.Constants.TYPE_CHAT_MESSAGE
+import sammy.com.Constants.TYPE_DRAW_DATA
+import sammy.com.data.Room
 import sammy.com.data.models.BaseModel
 import sammy.com.data.models.ChatMessage
+import sammy.com.data.models.DrawData
 import sammy.com.gson
+import sammy.com.server
 import sammy.com.session.DrawingSession
 import java.lang.Exception
+
+
+fun Route.gameWebSocketRoute() {
+    route("/ws/draw") {
+        standardWebSocket { socket, clientId, message, payload ->
+            when (payload) {
+                is DrawData -> {
+                    val room = server.rooms[payload.roomName] ?: return@standardWebSocket
+                    if (room.phase == Room.Phase.GAME_RUNNING){
+                        room.broadcastToAllExcept(message,clientId)
+                    }
+
+                }
+
+                is ChatMessage -> {
+
+                }
+            }
+        }
+    }
+}
 
 fun Route.standardWebSocket(
     handleFrame: suspend (
@@ -33,7 +59,8 @@ fun Route.standardWebSocket(
                     val message = frame.readText()
                     val jsonObject = JsonParser.parseString(message).asJsonObject
                     val type = when (jsonObject.get("type").asString) {
-                        Constants.TYPE_CHAT_MESSAGE -> ChatMessage::class.java
+                        TYPE_CHAT_MESSAGE -> ChatMessage::class.java
+                        TYPE_DRAW_DATA -> DrawData::class.java
                         else -> BaseModel::class.java
                     }
                     val payload = gson.fromJson(message, type)
